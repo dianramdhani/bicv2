14dadf6dd29ff44ff3369b53d710213494dcb424
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-03-30 16:58:27 +0700

    update pie: add chart

--- src/app/modules/dashboard/page/dashboard/pie/pie.component.ts
+++ src/app/modules/dashboard/page/dashboard/pie/pie.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as Chart from 'chart.js';
 
 @Component({
   selector: 'app-pie',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pie.component.scss']
 })
 export class PieComponent implements OnInit {
+  @ViewChild('chart', { static: true }) ctx: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    this.drawChart();
   }
 
+  protected drawChart() {
+    const chart = new Chart(this.ctx.nativeElement, {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          data: [
+            52,
+            48,
+          ],
+          backgroundColor: [
+            '#007bff',
+            '#97a3b9',
+          ]
+        }],
+        labels: [
+          'Occupied',
+          'Free Capacity',
+        ]
+      },
+      options: {
+        responsive: false,
+        legend: {
+          position: 'bottom'
+        }
+      }
+    });
+  }
 }
