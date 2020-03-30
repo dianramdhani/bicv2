508da16f920e2d9caa26da221506ce051db139b4
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-03-30 16:03:48 +0700

    init graph, pie

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { GraphComponent } from './page/dashboard/graph/graph.component';
+import { PieComponent } from './page/dashboard/pie/pie.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, GraphComponent, PieComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule
