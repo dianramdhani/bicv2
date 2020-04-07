73771278bf0f5e0b0b93c00a9624aee43c74d7f5
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-04-07 15:23:07 +0700

    update graph: switch in and out

--- src/app/modules/dashboard/page/dashboard/graph/graph.component.ts
+++ src/app/modules/dashboard/page/dashboard/graph/graph.component.ts
@@ -32,20 +32,20 @@ export class GraphComponent implements OnInit {
       data: {
         datasets: [
           {
-            label: 'Out',
-            data: containers
-              .filter(container => container.location === 'Out')
-              .map(container => ({ t: new Date(container.date), y: container.count })),
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)'
-          },
-          {
             label: 'In',
             data: containers
               .filter(container => container.location === 'In')
               .map(container => ({ t: new Date(container.date), y: container.count })),
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
             borderColor: 'rgba(54, 162, 235, 1)'
+          },
+          {
+            label: 'Out',
+            data: containers
+              .filter(container => container.location === 'Out')
+              .map(container => ({ t: new Date(container.date), y: container.count })),
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)'
           }
         ]
       },
